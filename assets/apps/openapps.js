d2eb1b1125ac5d11cb9db678ae044a3743ff44e9
realtechnerd <40891074+realtechnerd@users.noreply.github.com>
2020-07-21 21:50:19 -0400

    commit

--- assets/apps/openapps.js
+++ assets/apps/openapps.js
@@ -1,6 +1,8 @@
 var appname = document.getElementById('appname'); //The Name of the App
 var theapp = document.getElementById("theapp"); //The Actual App
 var appwindow = document.getElementById("appwindow");
+let name;
+
 
 $( function() {
     $( "#appwindow" ).draggable({
@@ -8,10 +10,19 @@ $( function() {
     });
   } );
 function notePad() {
-  $(theapp).height(250 - 34)
+  name = 'Notepad';
+
+  $(theapp).height(250 - 34);
+
   $( appwindow ).width( 250 );
+  
   $( appwindow ).height( 250 );
-    theapp.src = "/notepad/";
-    $(appwindow).toggle("scale");
-    appname.innerHTML = "Notepad";
+  
+  theapp.src = "/notepad/";
+  
+  $(appwindow).toggle("scale", 250);
+    
+  $(appname).html(name);
+  
+  $("#notepad").toggleClass("activeapp");
 }
