4d8e0ba62d675dd78fd1e4b7d4a404e0b2fefd15
realtechnerd <40891074+realtechnerd@users.noreply.github.com>
2020-07-24 17:36:04 -0400

    New Commit, bug fixing

--- apps/notepad/notepad.js
+++ apps/notepad/notepad.js
@@ -1,29 +1,10 @@
-function notePad() {
-    name = 'Notepad';
-  
-    $( appwindow ).width( 700 );
-    
-    $( appwindow ).height( 400 );
-    
-    $(theapp).css("height", $(appwindow).height());
+const notepadapp = document.getElementById("notepadapp");
+const notepadframe = document.getElementById("notepadframe");
 
-    $( appwindow ).toggle(0);
-      
-    $( appname ).html(name);
-    
-    $( "#notepad" ).toggleClass("activeapp");
-
-    document.getElementById("close").onclick = closeNotePad;
-
-    document.getElementById("maxmini").onclick = maxNotePad;
+function toggleNotePad() {
+  openApp(notepadapp, 400, 700, notepadframe);
 }
-function closeNotePad() {
-  $( appwindow ).hide();
-  $( "#notepad" ).removeClass("activeapp");
-}
-function maxNotePad() {
-  $(appwindow).css("height", $(appwindow).height());
-  appwindow.style.width = "100%";
-  $(theapp).css("height", $(document).height());
-  maxFix(appwindow);
+function maxNotePadApp() {
+  document.getElementById("#notepad").classList.toggle("active");
+  maximizeApp(notepadapp);
 }
--- assets/apps/openapps.js
+++ assets/apps/openapps.js
@@ -1,42 +1,20 @@
-const appname = document.getElementById('appname'); //The Name of the App
-const theapp = document.getElementById("theapp"); //The Actual App
-const appwindow = document.getElementById("appwindow");
-const appdetails = document.getElementById("appdetails");
-const maxmini = document.getElementById("maxmini");
-let name;
-
-
 $( function() {
-    $( "#appwindow" ).draggable({
+    $( ".appwindow" ).draggable({
       iframeFix: true
     });
   } );
-function maxFix(app) {
-  app.style.top = "0";
-  app.style.left = "0"; 
-  app.style.borderRadius = "0";
-  app.style.position = "fixed";
+function openApp(app, height, width, appframe) {
+  $( app   ).height(height + 34);
+  $( app ).width(width);
+  $(appframe).css("height", $("app").height());
+  $( app ).toggle("fade", 100);
 }
-$(function() {
-  $(theapp).resizable({
-    start: function(event, ui) {
-      ui.element.append($("<div/>", {
-        id: "iframe-barrier",
-        css: {
-          position: "absolute",
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          "z-index": 10
-        }
-      }));
-    },
-    stop: function(event, ui) {
-      $("#iframe-barrier", ui.element).remove();
-    },
-    resize: function(event, ui) {
-      $("iframe", ui.element).width(ui.size.width).height(ui.size.height);
-    }
-  });
-});
+function maximizeApp(app) {
+  app.style.top = "0";
+  app.style.bottom = "0";
+  app.style.left = "0";
+  app.style.right = "0";
+  $(app).css("width", $(document).width());
+  $(app).css("height", $(document).height());
+  $("#theapp").css("height", $(app).height());
+}
