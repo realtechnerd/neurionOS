4bb207d51d25a6cfeaf5be90538545fad76266c8
realtechnerd <40891074+realtechnerd@users.noreply.github.com>
2020-07-23 20:29:46 -0400

    Window updates, package.json update

--- apps/notepad/notepad.js
+++ apps/notepad/notepad.js
@@ -1,28 +1,29 @@
 function notePad() {
     name = 'Notepad';
   
-    theapp.src = "/apps/notepad/index.html";
-  
-    $(theapp).height(400 - 34);
-  
     $( appwindow ).width( 700 );
     
     $( appwindow ).height( 400 );
     
-    $( appwindow ).toggle("size",350);
+    $(theapp).css("height", $(appwindow).height());
+
+    $( appwindow ).toggle(0);
       
     $( appname ).html(name);
     
     $( "#notepad" ).toggleClass("activeapp");
 
     document.getElementById("close").onclick = closeNotePad;
+
     document.getElementById("maxmini").onclick = maxNotePad;
 }
 function closeNotePad() {
-  $( appwindow ).hide("size",350);
+  $( appwindow ).hide();
   $( "#notepad" ).removeClass("activeapp");
 }
 function maxNotePad() {
-  $(appwindow).css("height", $(document).height());
+  $(appwindow).css("height", $(appwindow).height());
+  appwindow.style.width = "100%";
+  $(theapp).css("height", $(document).height());
   maxFix(appwindow);
 }
--- assets/apps/openapps.js
+++ assets/apps/openapps.js
@@ -1,6 +1,8 @@
 const appname = document.getElementById('appname'); //The Name of the App
 const theapp = document.getElementById("theapp"); //The Actual App
 const appwindow = document.getElementById("appwindow");
+const appdetails = document.getElementById("appdetails");
+const maxmini = document.getElementById("maxmini");
 let name;
 
 
@@ -9,11 +11,32 @@ $( function() {
       iframeFix: true
     });
   } );
-
-  
 function maxFix(app) {
   app.style.top = "0";
   app.style.left = "0"; 
   app.style.borderRadius = "0";
-  appname.style.display = "none";
+  app.style.position = "fixed";
 }
+$(function() {
+  $(theapp).resizable({
+    start: function(event, ui) {
+      ui.element.append($("<div/>", {
+        id: "iframe-barrier",
+        css: {
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          "z-index": 10
+        }
+      }));
+    },
+    stop: function(event, ui) {
+      $("#iframe-barrier", ui.element).remove();
+    },
+    resize: function(event, ui) {
+      $("iframe", ui.element).width(ui.size.width).height(ui.size.height);
+    }
+  });
+});
